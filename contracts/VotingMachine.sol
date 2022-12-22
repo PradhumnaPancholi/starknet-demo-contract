da44df9bb2dda33945e06fff6979ccd349e2e535
Pradhumna Pancholi <pnpancholi@protonmail.com>
2022-12-22 05:56:09 -0500

    initial commit

---
+++ contracts/VotingMachine.sol
@@ -0,0 +1,22 @@
+pragma solidity 0.8.10;
+
+/// @author Pradhumna Pancholi
+/// @title Voting Machine 
+
+//import OZ Ownable //
+contract VotingMachne {
+
+	//ToDo: struct for candidate type//
+
+	//ToDo: mapping fo candidate to voters
+
+	//Todo: list of people who voters
+
+	//ToDo: function to add a candidate - only owner
+
+	//ToDo: function to retrieve the list of all of the candidate - view
+
+	//ToDo: funtion to vote - one address == one vote 
+	// Also add to the list of voters//
+
+}
