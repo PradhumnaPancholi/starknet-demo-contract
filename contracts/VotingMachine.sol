ae1ad84a0d2303452f413e1f3519932dd6883091
Pradhumna Pancholi <pnpancholi@protonmail.com>
2022-12-23 04:53:32 -0500

    minor progress in the contract

--- contracts/VotingMachine.sol
+++ contracts/VotingMachine.sol
@@ -1,3 +1,4 @@
+//SPDX-License-Identifier:  GPL-3.0
 pragma solidity 0.8.10;
 
 /// @author Pradhumna Pancholi
@@ -6,13 +7,28 @@ pragma solidity 0.8.10;
 //import OZ Ownable //
 contract VotingMachne {
 
-	//ToDo: struct for candidate type//
+	/// @notice struct for candidate type
+	struct Candidate{
+		uint8 id;
+		string name;
+	}
 
-	//ToDo: mapping fo candidate to voters
-
-	//Todo: list of people who voters
+	/// @notice list of candidates
+	uint8 candidates[] = 0;
+	
+	/// @notice for election period
+	uint256 public electionPeriod = 1 days;
+	/// @notice mapping to count number of voters
+	mapping(uint8 => uint256) public votes;
+	
+	//list of people who voted
+	uint256[] public voters;
 
 	//ToDo: function to add a candidate - only owner
+	function addCandidate() public {
+		uint8 id =  
+		Candidate newCandidate = new 
+	}
 
 	//ToDo: function to retrieve the list of all of the candidate - view
 
